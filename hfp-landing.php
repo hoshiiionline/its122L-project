92f8fae374b9c77128318935c554394643ed4bf3
Kai-Tradewind <97167302+Kai-Tradewind@users.noreply.github.com>
2025-02-17 01:01:09 +0800

    Styled Landing
    -establishing design language
    -div layouts (hfp-landing)

---
+++ hfp-landing.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset ="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Kaizer Cabrera</title>
+        <link href="styling/styling-landing.css" rel="stylesheet" type="text/css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Bentham&family=Fjalla+One&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Jost:ital,wght@0,100..900;1,100..900&family=Lexend+Giga:wght@100..900&family=Oranienbaum&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    </head>
+    <body>
+
+    <nav class="navbar">
+        <ul class="list">
+            <li><a href="#">About</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">News</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+        <button class="search">Search</button>
+        <button class="menu">Menu</button>
+    </nav>
+
+
+
+    <div class = "landing-page">
+        <video autoplay muted plays-inline class="back-video">
+            <source src="resources/vtour2.mp4" type="video/mp4">
+        </video>
+        <div class="content">
+            <h1 class = "landing-heading">Holy Family Parish</h1>
+            <p class="landing-subheading">This is placeholder text for the landing page of this website.</p>
+        </div>
+    </div>
+
+    <br>
+
+    <div class="carousel-section">  
+        <div class ="carousel-heading">Parish Gallery</div>
+        <br><br>
+        <div class = "carousel-subheading">This is where the content will go.</div>
+    </div>
+
+
+    <div class="carousel-container">
+            <div class="slider">
+        
+                <span style="--i:1;"><img src="resources/gallery1.jpg" alt=""></span>
+                <span style="--i:2;"><img src="resources/gallery2.jpg" alt=""></span>
+                <span style="--i:3;"><img src="resources/gallery3.jpg" alt=""></span>
+                <span style="--i:4;"><img src="resources/gallery4.jpg" alt=""></span>
+                <span style="--i:5;"><img src="resources/gallery5.jpg" alt=""></span>
+            </div>
+        </div>
+
+
+
+    <div class="register-container">
+
+
+        <div class="left-side">
+        <h1>Register for Admin side</h1>
+        </div>
+
+
+        <div class="right-side">
+            This is where the content of the landing page will go.
+        </div>
+
+
+    </div>
+
+    </body>
+</html>
