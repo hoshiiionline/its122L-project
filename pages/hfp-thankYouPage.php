95cc9872875a6656adfadafeea16e471fda70fd1
Kai-Tradewind <kzrcabrera@mymail.mapua.edu.ph>
2025-03-14 21:08:47 +0800

    QOL change - ThankU Message
    Updated Colors/styling for thank U page.
    
    -use this as checkpoint for progress

--- pages/hfp-thankYouPage.php
+++ pages/hfp-thankYouPage.php
@@ -15,8 +15,8 @@ $referenceNo = $_SESSION['referenceNo'];
 </head>
 <body class="background1">
 <div class="container" style="border-radius: 2em">
-    <h2>THANK YOU</h2>
-    <h3 style="padding-top: 1em">Thank you! Your request has been received. A confirmation email with your booking details will be sent to your inbox shortly. If you have any questions, feel free to contact us.</h3>
+    <h2>THANK YOU!</h2>
+    <h3 style="padding-top: 1em">Your request has been received. A confirmation email with your booking details will be sent to your inbox shortly. If you have any questions, feel free to contact us.</h3>
     <br>
     <h3>Your reference no.: <?php echo $referenceNo?></h3>
     <h3 style="margin: 0; padding: 0; font-size: 1.5em;"><a class="back" href="hfp-dashboard.php">Return to dashboard</a></h3>
